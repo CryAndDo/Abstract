665244205137db51b0ddc63a1923333649ef774d
As <As@DESKTOP-1CSV55L>
2023-03-23 14:35:21 +0200

    Добавьте файлы проекта.

---
+++ Obeme2/Triad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obeme2
+{
+    abstract class Triad
+    {
+        public abstract Triad Addition1();
+    }
+}
